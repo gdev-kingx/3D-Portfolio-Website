2a4289c02bb0f92421be905a2d1ba2d6eb12a84d
R Sarveshvarn <151063233+gdev-kingx@users.noreply.github.com>
2024-06-05 16:55:15 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,264 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    asteroids,
+    spaceinvaders,
+    yoom,
+    threejs,
+} from "../assets";
+  
+export const navLinks = [
+    {
+        id: "about",
+        title: "About",
+    },
+    {
+        id: "work",
+        title: "Work",
+    },
+    {
+        id: "contact",
+        title: "Contact",
+    },
+];
+  
+const services = [
+    {
+        title: "Web Developer",
+        icon: web,
+    },
+    {
+        title: "React Native Developer",
+        icon: mobile,
+    },
+    {
+        title: "Backend Developer",
+        icon: backend,
+    },
+    {
+        title: "Content Creator",
+        icon: creator,
+    },
+];
+  
+const technologies = [
+    {
+        name: "HTML 5",
+        icon: html,
+    },
+    {
+        name: "CSS 3",
+        icon: css,
+    },
+    {
+        name: "JavaScript",
+        icon: javascript,
+    },
+    {
+        name: "TypeScript",
+        icon: typescript,
+    },
+    {
+        name: "React JS",
+        icon: reactjs,
+    },
+    {
+        name: "Redux Toolkit",
+        icon: redux,
+    },
+    {
+        name: "Tailwind CSS",
+        icon: tailwind,
+    },
+    {
+        name: "Node JS",
+        icon: nodejs,
+    },
+    {
+        name: "MongoDB",
+        icon: mongodb,
+    },
+    {
+        name: "Three JS",
+        icon: threejs,
+    },
+    {
+        name: "git",
+        icon: git,
+    },
+    {
+        name: "figma",
+        icon: figma,
+    },
+    {
+        name: "docker",
+        icon: docker,
+    },
+];
+  
+const experiences = [
+    {
+        title: "React.js Developer",
+        company_name: "Starbucks",
+        icon: starbucks,
+        iconBg: "#383E56",
+        date: "March 2020 - April 2021",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "React Native Developer",
+        company_name: "Tesla",
+        icon: tesla,
+        iconBg: "#E6DEDD",
+        date: "Jan 2021 - Feb 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Web Developer",
+        company_name: "Shopify",
+        icon: shopify,
+        iconBg: "#383E56",
+        date: "Jan 2022 - Jan 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "Meta",
+        icon: meta,
+        iconBg: "#E6DEDD",
+        date: "Jan 2023 - Present",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+  
+const testimonials = [
+    {
+        testimonial:
+            "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+        name: "Sara Lee",
+        designation: "CFO",
+        company: "Acme Co",
+        image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+        testimonial:
+            "I've never met a web developer who truly cares about their clients' success like Rick does.",
+        name: "Chris Brown",
+        designation: "COO",
+        company: "DEF Corp",
+        image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+        testimonial:
+            "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+        name: "Lisa Wang",
+        designation: "CTO",
+        company: "456 Enterprises",
+        image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+];
+  
+const projects = [
+    {
+        name: "Asteroids",
+        description: "Web-based game that is a remake of the 1979 Atari game Asteroids using HTML5 Canvas and OOP Javascript wit minimal graphics",
+        tags: [
+            {
+                name: "HTML 5",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "Javascript",
+                color: "green-text-gradient",
+            },
+            {
+                name: "Vite",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: asteroids,
+        source_code_link: "https://github.com/gdev-kingx/1979-Asteroids",
+    },
+    {
+        name: "Space Invaders",
+        description:
+            "Web Based Game which is my take on the Atari 'Space Invaders' Game using HTML5 Canvas and OOP Javascript with Advance Graphics and Super Weapons",
+        tags: [
+            {
+                name: "HTML 5",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "CSS 3",
+                color: "green-text-gradient",
+            },
+            {
+                name: "Javascript",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: spaceinvaders,
+        source_code_link: "https://github.com/gdev-kingx/SpaceInvaders.js-v2",
+    },
+    {
+        name: "Yoom",
+        description:
+            "A Replica of the popular video calling Web Based / App Based Application 'ZOOM' using NextJS and React Typescript. Advanced Full Stack Dev Project",
+        tags: [
+            {
+                name: "nextjs",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "typescript",
+                color: "green-text-gradient",
+            },
+            {
+                name: "tailwindcss",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: yoom,
+        source_code_link: "https://github.com/gdev-kigx/Yoom",
+    },
+];
+  
+export { services, technologies, experiences, testimonials, projects };
